d0fe6eba88be00d5738fdc70285c588a5c50d699
Mathavan Kaliyaperumal <mathucs@gmail.com>
2024-01-28 17:51:30 +0530

    -view binding enabled

--- app/src/main/java/com/workmanager/onetimerequest/TimeScheduleActivity.kt
+++ app/src/main/java/com/workmanager/onetimerequest/TimeScheduleActivity.kt
@@ -2,10 +2,15 @@ package com.workmanager.onetimerequest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.workmanager.onetimerequest.databinding.ActivityTimeScheduleBinding
 
 class TimeScheduleActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityTimeScheduleBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_time_schedule)
+        binding = ActivityTimeScheduleBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
